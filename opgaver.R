a6063fbe44acc486c72545956abf35215e8c9a8f
annelandgrebe <alandg14@student.aau.dk>
2018-09-19 15:55:31 +0200

    Opgaver 1 til PART 2 er lavet

--- opgaver.R
+++ opgaver.R
@@ -22,6 +22,31 @@ prod(c(1,2,3,4))
 #' • Does it handle negative numbers correctly? (hint: the stop function can be used 
 #' to give an error message)
 #' • Does it handle decimal numbers correctly?
-#' 
-#' 
-#' 
+
+
+#' PART 2
+#' EXERCISE 1
+#' I) Consider the built-in dataset cars
+#'    a) Make the design matrix X for a simple linear regression for cars (dist as 
+#'    a function of speed).
+
+X <- matrix(c(rep(1,length(cars$speed)),cars$speed),length(cars$speed),2)
+Y <- cars$dist
+
+#'    b) Estimate beta. Plot the data and the estimated line in the same figure.
+#'    (hint: the function abline is useful for plotting the line)
+    
+A <- (t(X)%*%X)
+B <- t(X)%*%Y
+b=solve(A,B)
+plot(cars)
+abline(b)
+
+#'    c) Estimate sigmaˆ2.
+s=(t(Y-X%*%b)%*%(Y-X%*%b))/(50-(1+1))    #Sigma^2
+
+#' II) Maybe a second order polynomial is better at capturing the relation between speed 
+#' and distance? 
+#' Redo exercise I with a second order polynomial. (Hint: curve may be useful)
+
+
